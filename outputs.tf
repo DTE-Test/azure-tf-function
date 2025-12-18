e178335f7961b98a6d76ad44bda842724d8ae487
SanthoshReddyPasham <santhoshreddyp2012@gmail.com>
2025-12-18 01:32:17 -0500

    updated code

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output "function_app_id" {
 output "function_app_default_hostname" {
   value = azurerm_linux_function_app.employee_function.default_hostname
 }
+
+output "principal_id" {
+  description = "The Service Principal / Managed Identity Object ID of the Function App"
+  value       = azurerm_function_app.example.identity[0].principal_id
+}
